19b4d166ea17ce97111e812e144444481933c453
Аскар Хабибуллин <a.khabibullin@innopolis.ru>
2021-09-28 15:14:20 +0300

    Changed the liveserver

--- hackaton-front/src/app/app-routing.module.ts
+++ hackaton-front/src/app/app-routing.module.ts
@@ -1,15 +1,1 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TablePageComponent } from './table-page/table-page.component';
 
-const routes: Routes = [
-  {path:"table1", component:TablePageComponent},
-  {path:"table2", component:TablePageComponent},
-  {path:"table3", component:TablePageComponent}
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
